54b033276ce141d4a8f2ffb89a41f986c02ef857
menglcai <menglcai@amd.com>
2026-01-23 15:16:28 +0800

    update

--- setup.py
+++ setup.py
@@ -1772,6 +1772,8 @@ def main() -> None:
         "utils/model_dump/*.mjs",
         "_dynamo/graph_break_registry.json",
         "tools/dynamo/gb_id_mapping.py",
+        "lib/rocm/**",
+        "lib/rocm/bin/.miopen/**",
     ]
 
     if not BUILD_LIBTORCH_WHL:
